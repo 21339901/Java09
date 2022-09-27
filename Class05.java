1d241b71995bc7aa99f6419016216635a1b4add0
21339901 <113246766+21339901@users.noreply.github.com>
2022-09-27 23:17:26 +0800

    Create Class05.java

---
+++ Class05.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class C {
+	public static void main(String[] args) {
+		
+	}
+}
+
+class CCount {
+	static int cnt = 0;
+	public CCount() {
+		count();
+	}
+	void count() {
+		cnt++;
+	}
+	void setZero() {
+		cnt = 0;
+	}
+	void setvalue(int n) {
+		cnt = n;
+	}		
+}
