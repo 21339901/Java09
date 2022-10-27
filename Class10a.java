5d71e0d46500916eaafc064002a1860271bc9da4
21339901 <113246766+21339901@users.noreply.github.com>
2022-10-27 14:57:53 +0800

    Create Class10a.java

---
+++ Class10a.java
@@ -0,0 +1,33 @@
+package Java11;
+
+public class Class10a {
+	
+	public static void main(String args[]) {
+		CRational aaa = new CRational();
+		aaa.setN(2);
+		aaa.setD(5);
+		aaa.show();
+	}
+}
+
+class CRational{
+	public int n;
+	public int d;
+	
+	public void setN(int num) { //son
+		n = num;
+	}
+	
+	public void setD(int num) { //mother
+		d = num;
+	}
+	
+	public void setND(int num,int den) {
+		n = num;
+		d = den;
+	}
+	
+	public void show() {
+		System.out.println(n+"/"+d);
+	}
+}
